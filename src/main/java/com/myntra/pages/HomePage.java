9fb9983e00288edfe1a478a4399777b1551e7be7
Arif-Iquba'l <arif04102001@gmail.com>
2026-03-18 15:10:50 +0530

    [Arif Iqubal] : added homepage, excelfileReader and writer utils, and screenshot utils also updated tabSwitching logic

--- src/main/java/com/myntra/pages/HomePage.java
+++ src/main/java/com/myntra/pages/HomePage.java
@@ -1,12 +1,64 @@
 package com.myntra.pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import org.apache.logging.log4j.Logger;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.myntra.utils.*;
+
+import java.util.List;
+
 public class HomePage {
 
-	public int cartIconReader() {
-		return 0;
+	WebDriver driver;
+	Logger log = LoggerUtil.getLogger(HomePage.class);
+
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
-	public void addProduct() {
+	@FindBy(xpath = "//input[@placeholder='Search for products, brands and more']")
+	WebElement searchBox;
 
+	public void addProduct(String path, ExtentTest test) {
+
+		List<String> products = ExcelReader.getProductNames(path);
+
+		for (String product : products) {
+
+			try {
+				log.info("Searching product: " + product);
+				test.info("Searching: " + product);
+
+				WaitUtils.waitForVisibility(driver, searchBox);
+				searchBox.clear();
+				searchBox.sendKeys(product);
+				searchBox.sendKeys(Keys.ENTER);
+
+				SearchPage searchPage = new SearchPage(driver);
+				searchPage.searchPage();
+
+				String parent = WindowSwitch.toChild(driver);
+
+				ProductPage productPage = new ProductPage(driver);
+				productPage.addToBag();
+
+				log.info("Product added: " + product);
+				test.pass("Added: " + product);
+
+				driver.close();
+				WindowSwitch.toParent(driver, parent);
+
+			} catch (Exception e) {
+
+				log.error("Failed product: " + product);
+
+				String img = ScreenshotUtil.capture(driver, product);
+
+				test.fail("Failed: " + product).addScreenCaptureFromPath(img);
+			}
+		}
 	}
 }
